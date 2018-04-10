d0d38eff652a77e95d203985a082ff157486953c
Declan Vong <declan@canva.com>
2018-04-10 13:58:24 +1000

    Someone malicious or accidentally removes code

--- SomeFile.js
+++ SomeFile.js
@@ -1,3 +1,3 @@
 const someLegitCode = 6;
-const iReallyDontWantToLoseMyWork = 'it would be a shame';
+const iReallyDontWantToLoseMyWork = 'it would be a shame if someone were to wipe your changes';
 
